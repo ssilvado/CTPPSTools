e9c29f857a82e11d6430f49c30ca0a81bd9cd315
Ewerton Belchior <ewerton.belchior@cern.ch>
2016-06-25 09:13:40 -0300

    modified

--- Filters/python/doubleArmFilter_cfi.py
+++ Filters/python/doubleArmFilter_cfi.py
@@ -2,6 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 doubleArmFilter = cms.EDFilter( 'DoubleArmFilter'
                                , vertices = cms.InputTag('offlineSlimmedPrimaryVertices')
-                               , ppsLabel = cms.string('PPSReco')
                                , tofRes = cms.uint32(50) # picoseconds
                               )
