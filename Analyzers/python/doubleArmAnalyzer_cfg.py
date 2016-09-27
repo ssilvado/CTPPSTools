22b66f6a37dfa4c4d3aa3759c2d2d4a47a56bbaf
ebelchio <ewerton.belchior@cern.ch>
2016-09-27 11:36:05 -0300

    Create doubleArmAnalyzer_cfg.py

---
+++ Analyzers/python/doubleArmAnalyzer_cfg.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.MessageLogger = cms.Service("MessageLogger"
+                                   , destinations = cms.untracked.vstring('cout')
+                                   , cout = cms.untracked.PSet(
+                                           threshold = cms.untracked.string('WARNING') 
+                                            )
+)
+
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+
+process.maxEvents = cms.untracked.PSet(10)
+
+process.source = cms.Source( "PoolSource"
+                            , fileNames = cms.untracked.vstring('file:file.root')
+                            , duplicateCheckMode = cms.untracked.string("noDuplicateCheck")
+                           )
+
+process.analyzer = cms.EDAnalyzer( 'DoubleArmAnalyzer'
+                                  , vertices = cms.InputTag('offlineSlimmedPrimaryVertices')
+                                  , tofRes = cms.double(20) # picoseconds
+                                 )
+
+process.p = cms.Path(process.analyzer)
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string('out.root') )
