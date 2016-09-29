a1c27a0e504dc8ae461e42c93553fc98ad54dcc1
ebelchio <ewerton.belchior@cern.ch>
2016-09-28 22:39:13 -0300

    Update DoubleArmAnalyzer.cc

--- Analyzers/plugins/DoubleArmAnalyzer.cc
+++ Analyzers/plugins/DoubleArmAnalyzer.cc
@@ -2,9 +2,7 @@
 
 // class members definiton
 DoubleArmAnalyzer::DoubleArmAnalyzer(const edm::ParameterSet& cfg) :
-  vertices_tk (consumes<DataTypes::VertexCollection>(cfg.getParameter<edm::InputTag>("vertices")))
-, ppsreco_tk(consumes<DataTypes::PPSRecoCollection>(edm::InputTag("ppssim","PPSReco")))
-, tofRes(cfg.getParameter<double>("tofRes")*pow(10,-12)) // in seconds
+  ppsreco_tk(consumes<DataTypes::PPSRecoCollection>(edm::InputTag("ppssim","PPSReco")))
 {
    //now do what ever initialization is needed
    usesResource("TFileService");
@@ -14,18 +12,16 @@ DoubleArmAnalyzer::DoubleArmAnalyzer(const edm::ParameterSet& cfg) :
 void DoubleArmAnalyzer::beginJob()
 {
   // initialize
-  doubles["pps_fwd_ntracks"] = 99999;
-  doubles["pps_bkw_ntracks"] = 99999;
-  doubles["pps_fwd_matched_tracks"] = 99999;
-  doubles["pps_bkw_matched_tracks"] = 99999;
-  doubles["pps_matched_vertices"] = 99999;
+  vecdoubles["pps_fwd_t"] = new std::vector<double>();
+  vecdoubles["pps_fwd_xi"] = new std::vector<double>();
+  vecdoubles["pps_bkw_t"] = new std::vector<double>();
+  vecdoubles["pps_bkw_xi"] = new std::vector<double>();
 
   tree = fs->make<TTree>( "Events","Events" );
-  tree->Branch("pps_fwd_ntracks",&doubles["pps_fwd_ntracks"],"pps_fwd_ntracks/D");
-  tree->Branch("pps_bkw_ntracks",&doubles["pps_bkw_ntracks"],"pps_bkw_ntracks/D");
-  tree->Branch("pps_fwd_matched_tracks",&doubles["pps_fwd_matched_tracks"],"pps_fwd_matched_tracks/D");
-  tree->Branch("pps_bkw_matched_tracks",&doubles["pps_bkw_matched_tracks"],"pps_bkw_matched_tracks/D");
-  tree->Branch("pps_matched_vertices",&doubles["pps_matched_vertices"],"pps_matched_vertices/D");
+  tree->Branch("pps_fwd_t","std::vector<double>",&vecdoubles["pps_fwd_t"]);
+  tree->Branch("pps_fwd_xi","std::vector<double>",&vecdoubles["pps_fwd_xi"]);
+  tree->Branch("pps_bkw_t","std::vector<double>",&vecdoubles["pps_bkw_t"]);
+  tree->Branch("pps_bkw_xi","std::vector<double>",&vecdoubles["pps_bkw_xi"]);
 }
 
 void DoubleArmAnalyzer::endJob() 
@@ -37,110 +33,34 @@ void DoubleArmAnalyzer::endJob()
 void DoubleArmAnalyzer::analyze(const edm::Event& event, const edm::EventSetup& setup)
 {
   // reset variables in each event
-  doubles["pps_fwd_ntracks"] = 99999;
-  doubles["pps_bkw_ntracks"] = 99999;
-  doubles["pps_fwd_matched_tracks"] = 99999;
-  doubles["pps_bkw_matched_tracks"] = 99999;
-  doubles["pps_matched_vertices"] = 99999;
+  vecdoubles["pps_fwd_t"]->clear();
+  vecdoubles["pps_fwd_xi"]->clear();
+  vecdoubles["pps_bkw_t"]->clear();
+  vecdoubles["pps_bkw_xi"]->clear();
 
-  event.getByToken(vertices_tk, vertices); 
   event.getByToken(ppsreco_tk, ppsreco);
    
   // check collections
-  if( !vertices.isValid()
-   || !ppsreco.isValid()
+  if( !ppsreco.isValid()
     ) {
     throw cms::Exception("DoubleArmAnalyzer") << "\n +" << __FILE__ << ":" << __LINE__ << ":"
     << "\n analyzer(): One of the following collections could not be retrieved" 
     << "\n analyzer(): from root file:"
-    << "\n analyzer(): vertices.isValid()=" << vertices.isValid()
     << "\n analyzer(): ppsreco.isValid()=" << ppsreco.isValid()
     << "\n analyzer(): Aborting.\n\n";
   }
-   
-  // check primary vertex
-  if (!vertices->size()) {
-    edm::LogWarning("DoubleArmAnalyzer") << "\n +" << __FILE__ << ":" << __LINE__ << ":"
-    << "\n analyzer(): No primary vertex in this event! Skiping event...";
-    return;
-  }
 
-  // check pps vertex
-  if (!ppsreco->Vertices->size()) {
-    edm::LogWarning("DoubleArmAnalyzer") << "\n +" << __FILE__ << ":" << __LINE__ << ":"
-    << "\n analyzer(): No PPS vertex in this event! Skiping event...";
-    return;
-  }
-
-  // signal pv_z (cm)
-  const double pv_z = vertices->at(0).z();
-
-  // speed of light (m/s)
-  const double c = 3.*pow(10,8);
-
-  // pps_z resolution (cm)
-  const double ppsz_resolution = ((c/2.)*sqrt(2)*tofRes)*pow(10,2);
-
-
-  std::vector<std::pair<size_t,size_t> > tracks;
-  std::set<size_t> fwd_tracks;
-  std::set<size_t> bkw_tracks;
 
   for (size_t i=0; i < ppsreco->ArmF.Tracks.size(); i++) {
-    for (size_t j=0; j < ppsreco->ArmB.Tracks.size(); j++) {
-
-      double fwd_tof = ppsreco->ArmF.Tracks.at(i).ToF.ToF;
-      double bkw_tof = ppsreco->ArmB.Tracks.at(j).ToF.ToF;
-		
-      if(fwd_tof==0 || bkw_tof==0) continue;
-//      edm::LogWarning("DoubleArmAnalyzer")
-//      << "\n analyzer(): ppsreco->ArmF.Tracks.at(" << i << ").ToF.ToF = " << ppsreco->ArmF.Tracks.at(i).ToF.ToF	    
-//      << "\n analyzer(): ppsreco->ArmB.Tracks.at(" << j << ").ToF.ToF = " << ppsreco->ArmB.Tracks.at(j).ToF.ToF;
-	    
-      // delta ToF (s)
-      const double deltaTof = (bkw_tof-fwd_tof)*pow(10,-9);
-
-      // pps z (cm)
-      const double pps_z = ((c/2.)*deltaTof)*pow(10,2);
-
-      const double zppsmax = pps_z + ppsz_resolution;
-      const double zppsmin = pps_z - ppsz_resolution;	
-	
-      if(zppsmin < pv_z && pv_z < zppsmax) {
-        tracks.push_back(std::make_pair(i,j));
-        fwd_tracks.insert(i); // do not duplicate entry
-        bkw_tracks.insert(j); // do not duplicate entry
-
-        edm::LogWarning("DoubleArmAnalyzer")
-        << "\n analyzer(): PPS vertex matched to PV:\n"
-        << "\n analyzer(): track combination: fwd(" << i << "),bkw(" << j << ")"
-        << "\n analyzer(): pv_z: " << pv_z
-        << "\n analyzer(): pps_vertex_z: " << pps_z
-        << "\n analyzer(): pps_vertex_z-pv_z [mm]: " << (pps_z-pv_z)*10;
-      }
-    }
+    if(ppsreco->ArmF.Tracks.at(i).ToF.ToF==0) continue;
+    vecdoubles["pps_fwd_t"]->push_back(ppsReco->ArmF.Tracks.at(i).t);
+    vecdoubles["pps_fwd_xi"]->push_back(ppsreco->ArmF.Tracks.at(i).xi);
   }
-
-
-  // total tracks
-  doubles["pps_fwd_ntracks"] = ppsreco->ArmF.Tracks.size();
-  doubles["pps_bkw_ntracks"] = ppsreco->ArmB.Tracks.size();
-
-  // matched vertices
-  doubles["pps_matched_vertices"] = tracks.size();
-
-  // matched tracks
-  doubles["pps_fwd_matched_tracks"] = fwd_tracks.size();
-  doubles["pps_bkw_matched_tracks"] = bkw_tracks.size();
-
-  if(tracks.size())
-    edm::LogWarning("DoubleArmAnalyzer")
-    << "\n\n analyzer(): -----------------RESULTS-----------------\n"
-    << "\n analyzer(): forward tracks  : total   =" << doubles["pps_fwd_ntracks"]
-    << "\n analyzer(): backward tracks : total   =" << doubles["pps_bkw_ntracks"]
-    << "\n analyzer(): forward tracks  : matched =" << doubles["pps_fwd_matched_tracks"]
-    << "\n analyzer(): backward tracks : matched =" << doubles["pps_bkw_matched_tracks"]
-    << "\n analyzer(): pps vertices    : matched =" << doubles["pps_matched_vertices"];
+  for (size_t i=0; i < ppsreco->ArmB.Tracks.size(); i++) {
+    if(ppsreco->ArmB.Tracks.at(i).ToF.ToF==0) continue;
+    vecdoubles["pps_bkw_t"]->push_back(ppsReco->ArmB.Tracks.at(i).t);
+    vecdoubles["pps_bkw_xi"]push_back(ppsreco->ArmB.Tracks.at(i).xi);
+	}
 
   tree->Fill();
 
