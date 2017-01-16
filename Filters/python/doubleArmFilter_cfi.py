c0c9a2457d17944db901584584e65aad55a01766
uerj-cms-cep-studies <uerj.cms.cep.studies@gmail.com>
2017-01-15 23:42:39 -0200

    Update doubleArmFilter_cfi.py

--- Filters/python/doubleArmFilter_cfi.py
+++ Filters/python/doubleArmFilter_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 doubleArmFilter = cms.EDFilter( 'DoubleArmFilter'
                                , vertices = cms.InputTag('offlineSlimmedPrimaryVertices')
-                               , tofRes = cms.double(20) # picoseconds
+                               , tofRes = cms.double(10) # picoseconds
                               )
 
 #MessageLogger = cms.Service("MessageLogger")
